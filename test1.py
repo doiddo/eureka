c12f75dac96eeeaa2711fabc66f590c68ea4dbf0
doiddo <101390093+doiddo@users.noreply.github.com>
2022-03-18 20:49:43 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,6 +4,8 @@ reverse_num=n[::-1]             #슬라이싱을 이용해 n을 거꾸로 바꿔
 
 remove_num=reverse_num.lstrip("0")      #선행하는 0들을 제거하는 lstrip함수를 이용해 선행하는 0제거
 
+print(origin_num)
+
 
 
 print(remove_num)     #출력
